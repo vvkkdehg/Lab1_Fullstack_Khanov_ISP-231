03c33c61d3705f2539e258e8967c3f36f6e30737
Vlados <hanovvladik@mail.ru>
2026-01-18 14:02:47 +0300

    Логика продолжения

--- Lab1_ISRPO_Khanov_ISP-231/backend/program.cs
+++ Lab1_ISRPO_Khanov_ISP-231/backend/program.cs
@@ -8,3 +8,19 @@ Console.WriteLine($"Привет, {name}");
 Console.WriteLine();
 string dayOfWeek = DateTime.Now.ToString("dddd");
 Console.WriteLine($"Текущий день недели: {dayOfWeek}");
+Console.WriteLine();
+Console.Write("Хотите продолжить? (y/n): ");
+string answer = Console.ReadLine()?.ToLower();
+if (answer == "y")
+{
+    Console.WriteLine("Продолжаем");
+}
+else if (answer == "n")
+{
+    Console.WriteLine("Завершение работы...");
+    Console.WriteLine("Работа завершена");
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
